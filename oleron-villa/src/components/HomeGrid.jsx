4bcadeb4f0aada6e1de72b301b45e4b37961b0ef
am-MongoDB <andrew.morgan@mongodb.com>
2024-08-30 14:58:06 +0100

    Sort out the grid layout for the home page

--- oleron-villa/src/components/HomeGrid.jsx
+++ oleron-villa/src/components/HomeGrid.jsx
@@ -6,7 +6,28 @@ export default function HomeGrid({action}) {
   const { language } = useContext(UserContext);
 
   return (
-    <div className='responsive-text'>
+    <>
+    <div className='responsive-grid'>
+      <InfoBox 
+        imageSrc="/images/outside/Oleron_Garden_TT_1900.jpg"
+        alt={language === "fr" ? "Un photo" : "A photo"}
+        action={action}
+        route={language === "fr" ? "/booking-fr" : "/booking"} // TODO: Change route
+        body={`
+          <h3>Facilities</h3>
+          <p>You should find everything you need for a comfortable and enjoyable stay. The kitchen is well equipped with dish-washer, microwave, oven, crockery, etc. In the lounge there is a hi-def TV together with both English and French programs - together with a great sound system. Wireless broadband is available throughout the house. Outside there is garden furniture, a barbecue and some bikes that you can borrow.</p>
+        `}
+      />
+      <InfoBox 
+        imageSrc="/images/outside/Oleron_Garden_TT_1900.jpg"
+        alt={language === "fr" ? "Un photo" : "A photo"}
+        action={action}
+        route={language === "fr" ? "/booking-fr" : "/booking"} // TODO: Change route
+        body={`
+          <h3>Facilities</h3>
+          <p>You should find everything you need for a comfortable and enjoyable stay. The kitchen is well equipped with dish-washer, microwave, oven, crockery, etc. In the lounge there is a hi-def TV together with both English and French programs - together with a great sound system. Wireless broadband is available throughout the house. Outside there is garden furniture, a barbecue and some bikes that you can borrow.</p>
+        `}
+      />
       <InfoBox 
         imageSrc="/images/outside/Oleron_Garden_TT_1900.jpg"
         alt={language === "fr" ? "Un photo" : "A photo"}
@@ -18,5 +39,38 @@ export default function HomeGrid({action}) {
         `}
       />
     </div>
+    <div className='responsive-grid'>
+    <InfoBox 
+      imageSrc="/images/outside/Oleron_Garden_TT_1900.jpg"
+      alt={language === "fr" ? "Un photo" : "A photo"}
+      action={action}
+      route={language === "fr" ? "/booking-fr" : "/booking"} // TODO: Change route
+      body={`
+        <h3>Facilities</h3>
+        <p>You should find everything you need for a comfortable and enjoyable stay. The kitchen is well equipped with dish-washer, microwave, oven, crockery, etc. In the lounge there is a hi-def TV together with both English and French programs - together with a great sound system. Wireless broadband is available throughout the house. Outside there is garden furniture, a barbecue and some bikes that you can borrow.</p>
+      `}
+    />
+    <InfoBox 
+      imageSrc="/images/outside/Oleron_Garden_TT_1900.jpg"
+      alt={language === "fr" ? "Un photo" : "A photo"}
+      action={action}
+      route={language === "fr" ? "/booking-fr" : "/booking"} // TODO: Change route
+      body={`
+        <h3>Facilities</h3>
+        <p>You should find everything you need for a comfortable and enjoyable stay. The kitchen is well equipped with dish-washer, microwave, oven, crockery, etc. In the lounge there is a hi-def TV together with both English and French programs - together with a great sound system. Wireless broadband is available throughout the house. Outside there is garden furniture, a barbecue and some bikes that you can borrow.</p>
+      `}
+    />
+    <InfoBox 
+      imageSrc="/images/outside/Oleron_Garden_TT_1900.jpg"
+      alt={language === "fr" ? "Un photo" : "A photo"}
+      action={action}
+      route={language === "fr" ? "/booking-fr" : "/booking"} // TODO: Change route
+      body={`
+        <h3>Facilities</h3>
+        <p>You should find everything you need for a comfortable and enjoyable stay. The kitchen is well equipped with dish-washer, microwave, oven, crockery, etc. In the lounge there is a hi-def TV together with both English and French programs - together with a great sound system. Wireless broadband is available throughout the house. Outside there is garden furniture, a barbecue and some bikes that you can borrow.</p>
+      `}
+    />
+  </div>
+  </>
   );
 }
