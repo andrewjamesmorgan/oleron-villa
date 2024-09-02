46a82976da68a623af810d4bcf067e7f8e2bc048
am-MongoDB <andrew.morgan@mongodb.com>
2024-09-02 13:02:43 +0100

    Initial sample booking data

---
+++ oleron-villa/src/temp/bookingData.jsx
@@ -0,0 +1,137 @@
+export let bookingData = [
+    {
+      property: 'Oleron Villa',
+      start: new Date('2024-07-06'),
+      end: new Date('2024-07-13'),
+      booked: true,
+      booking: {
+        name: 'Fred West',
+        email: 'fred@west.com',
+        phone: '+44-87342282797',
+        comments: 'Test booking',
+        source: 'Direct'
+      },
+      price: 1200,
+      archived: false
+    },
+    {
+      property: 'Oleron Villa',
+      start: new Date('2024-07-13'),
+      end: new Date('2024-07-20'),
+      booked: true,
+      booking: {
+        name: 'Booking 2',
+        email: 'booking2@example.com',
+        phone: '+44-87342282797',
+        comments: 'Test booking',
+        source: 'Direct'
+      },
+      price: 1200,
+      archived: false
+    },
+    {
+      property: 'Oleron Villa',
+      start: new Date('2024-07-20'),
+      end: new Date('2024-07-27'),
+      booked: true,
+      booking: {
+        name: 'Booking 3',
+        email: 'booking3@example.com',
+        phone: '+44-87342282797',
+        comments: 'Test booking',
+        source: 'Direct'
+      },
+      price: 1200,
+      archived: false
+    },
+    {
+      property: 'Oleron Villa',
+      start: new Date('2024-07-27'),
+      end: new Date('2024-08-03'),
+      booked: true,
+      booking: {
+        name: 'Booking 4',
+        email: 'booking4@example.com',
+        phone: '+44-87342282797',
+        comments: 'Test booking',
+        source: 'Direct'
+      },
+      price: 1200,
+      archived: false
+    },
+    {
+      property: 'Oleron Villa',
+      start: new Date('2024-08-03'),
+      end: new Date('2024-08-10'),
+      booked: true,
+      booking: {
+        name: 'Booking 5',
+        email: 'booking5@example.com',
+        phone: '+44-87342282797',
+        comments: 'Test booking',
+        source: 'Direct'
+      },
+      price: 1200,
+      archived: false
+    },
+    {
+      property: 'Oleron Villa',
+      start: new Date('2024-08-10'),
+      end: new Date('2024-08-17'),
+      booked: true,
+      booking: {
+        name: 'Booking 6',
+        email: 'booking6@example.com',
+        phone: '+44-87342282797',
+        comments: 'Test booking',
+        source: 'Direct'
+      },
+      price: 1200,
+      archived: false
+    },
+    {
+      property: 'Oleron Villa',
+      start: new Date('2024-08-17'),
+      end: new Date('2024-08-24'),
+      booked: true,
+      booking: {
+        name: 'Booking 7',
+        email: 'booking7@example.com',
+        phone: '+44-87342282797',
+        comments: 'Test booking',
+        source: 'Direct'
+      },
+      price: 1200,
+      archived: false
+    },
+    {
+      property: 'Oleron Villa',
+      start: new Date('2024-08-24'),
+      end: new Date('2024-08-31'),
+      booked: true,
+      booking: {
+        name: 'Booking 8',
+        email: 'booking8@example.com',
+        phone: '+44-87342282797',
+        comments: 'Test booking',
+        source: 'Direct'
+      },
+      price: 1200,
+      archived: false
+    },
+    {
+      property: 'Oleron Villa',
+      start: new Date('2024-08-31'),
+      end: new Date('2024-09-07'),
+      booked: true,
+      booking: {
+        name: 'Booking 9',
+        email: 'booking9@example.com',
+        phone: '+44-87342282797',
+        comments: 'Test booking',
+        source: 'Direct'
+      },
+      price: 1200,
+      archived: false
+    }
+  ];
