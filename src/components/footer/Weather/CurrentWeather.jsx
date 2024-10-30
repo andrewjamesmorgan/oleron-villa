24e7fbc0d73a2e74704779687fc5f7b373dc380b
am-MongoDB <andrew.morgan@mongodb.com>
2024-10-30 15:11:38 +0000

    More work on weather summary

--- src/components/footer/Weather/CurrentWeather.jsx
+++ src/components/footer/Weather/CurrentWeather.jsx
@@ -1,14 +1,28 @@
 import WeatherIcon from './WeatherIcon';
 import Temp from './Temp';
-export default function CurrentWeather({currentWeather}) {
+import WindIndicator from './WindIndicator';
+
+export default function CurrentWeather({currentWeather, dailyWeather}) {
   return (
     <div className="current-weather">
-      <WeatherIcon 
-        icon={currentWeather.weather[0].icon}
-        description={currentWeather.weather[0].description}
-      />
-      <Temp temp={currentWeather.temp}/>
+      <div>
+        <WeatherIcon 
+          icon={currentWeather.weather[0].icon}
+          description={currentWeather.weather[0].description}
+        />
+        <Temp temp={currentWeather.temp} suffix="true"/>
+      </div>
+      <div>
+        <div id="weather-current-summary">
+          <Temp 
+            temp={dailyWeather.temp.min}/>-<Temp 
+            temp={dailyWeather.temp.max} suffix="true"/>
+          <WindIndicator 
+            speed={currentWeather.wind_speed}
+            degrees={currentWeather.wind_deg}
+          />
+        </div>
+      </div>
     </div>
-
   );
 }
