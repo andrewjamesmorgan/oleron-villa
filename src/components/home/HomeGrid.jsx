3699909c9c933e088bd2a6caf6c48cd4cf113282
am-MongoDB <andrew.morgan@mongodb.com>
2024-11-10 19:30:09 +0000

    Started on outdoors page. Less horizontal padding for footer on narrow screens

--- src/components/home/HomeGrid.jsx
+++ src/components/home/HomeGrid.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { UserContext } from '../../App';
 import InfoBox from '../InfoBox';
 import InsideHouseCell from './homeGrid/InsideHouseCell';
+import OutsideHouseCell from './homeGrid/OutsideHouseCell';
 
 export default function HomeGrid({action}) {
   const { language } = useContext(UserContext);
@@ -10,16 +11,8 @@ export default function HomeGrid({action}) {
     <>
     <div className='responsive-grid'>
       <InsideHouseCell action={action} />
-      <InfoBox 
-        imageSrc="../../images/outside/Oleron_Garden_TT_1900.jpg"
-        alt={language === "fr" ? "Un photo" : "A photo"}
-        action={action}
-        route={language === "fr" ? "/bookings-fr" : "/bookings"} // TODO: Change route
-        body={`
-          <h3>Facilities</h3>
-          <p>You should find everything you need for a comfortable and enjoyable stay. The kitchen is well equipped with dish-washer, microwave, oven, crockery, etc. In the lounge there is a hi-def TV together with both English and French programs - together with a great sound system. Wireless broadband is available throughout the house. Outside there is garden furniture, a barbecue and some bikes that you can borrow.</p>
-        `}
-      />
+      <OutsideHouseCell action={action} />
+      
       <InfoBox 
         imageSrc="../../images/outside/Oleron_Garden_TT_1900.jpg"
         alt={language === "fr" ? "Un photo" : "A photo"}
---
+++ src/components/outside/OutsideGrid.jsx
@@ -0,0 +1,15 @@
+export default function InsideGrid({action}) {
+  return (
+    <>
+    <div className='responsive-grid'>
+
+    </div>
+    <div className='responsive-grid'>
+
+    </div>
+    <div className='responsive-grid'>
+
+    </div>
+  </>
+  );
+}
