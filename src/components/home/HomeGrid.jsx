ca37ddba2fdcd1409f5cdb6becdfd0b336e0383f
am-MongoDB <andrew.morgan@mongodb.com>
2024-12-02 09:20:46 +0000

    Add link/tile from home page to the bookings page

--- src/components/home/HomeGrid.jsx
+++ src/components/home/HomeGrid.jsx
@@ -5,6 +5,7 @@ import InsideHouseCell from './homeGrid/InsideHouseCell';
 import OutsideHouseCell from './homeGrid/OutsideHouseCell';
 import LocationCell from './homeGrid/LocationCell';
 import FacilitiesCell from './homeGrid/FacilitiesCell';
+import BookingCell from './homeGrid/BookingCell';
 
 export default function HomeGrid({action}) {
   const { language } = useContext(UserContext);
@@ -18,16 +19,7 @@ export default function HomeGrid({action}) {
     </div>
     <div className='responsive-grid'>
       <LocationCell action={action} />
-    <InfoBox 
-      imageSrc="../../images/outside/Oleron_Garden_TT_1900.jpg"
-      alt={language === "fr" ? "Un photo" : "A photo"}
-      action={action}
-      route={language === "fr" ? "/bookings-fr" : "/bookings"} // TODO: Change route
-      body={`
-        <h3>Facilities</h3>
-        <p>You should find everything you need for a comfortable and enjoyable stay. The kitchen is well equipped with dish-washer, microwave, oven, crockery, etc. In the lounge there is a hi-def TV together with both English and French programs - together with a great sound system. Wireless broadband is available throughout the house. Outside there is garden furniture, a barbecue and some bikes that you can borrow.</p>
-      `}
-    />
+      <BookingCell action={action} />
     <InfoBox 
       imageSrc="../../images/outside/Oleron_Garden_TT_1900.jpg"
       alt={language === "fr" ? "Un photo" : "A photo"}
