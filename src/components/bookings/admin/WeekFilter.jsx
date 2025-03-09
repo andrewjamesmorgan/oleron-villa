f8937914bc6420c76c44b0d2b21c4f89d945e65e
am-MongoDB <andrew.morgan@mongodb.com>
2025-03-09 13:09:50 +0000

    Fix dependency array in WeekFilter to include applyFilter function (removes lint warning)

--- src/components/bookings/admin/WeekFilter.jsx
+++ src/components/bookings/admin/WeekFilter.jsx
@@ -35,8 +35,8 @@ export default function WeekFilter({ applyFilter }) {
   }
 
   useEffect(() => {
-    applyFilter(filter);
-  }, [filter]);
+    applyFilter(filter, );
+  }, [filter, applyFilter]);
 
   return (
     <>
