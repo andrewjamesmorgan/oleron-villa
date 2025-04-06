b981fea011bdc68d5c7fe46c3917bdbc8c2a032d
Andrew Morgan <andrew.morgan@mongodb.com>
2025-04-06 05:40:57 +0000

    Add centerX and centerY props to CroppedImage for customizable cropping

--- src/components/CroppedImage.jsx
+++ src/components/CroppedImage.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-export default function CroppedImage({imageSrc, alt, action}) {
+export default function CroppedImage({ imageSrc, alt, action, centerX = 50, centerY = 50 }) {
+  // Overrides any styles associated with the class
+  const cropStyle = {
+    objectPosition: `${centerX}% ${centerY}%`
+  };
 
   return (
     <div className='croppedImage'>
-      <img src={imageSrc} alt={alt} className='croppedImage' 
-      onClick={action}
+      <img
+        src={imageSrc}
+        alt={alt}
+        className='croppedImage'
+        style={cropStyle}
+        onClick={action}
       />
     </div>
   );
