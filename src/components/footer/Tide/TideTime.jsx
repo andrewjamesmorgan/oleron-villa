8487aedfa00606442af457e4e2b040f7b4cd4618
Andrew Morgan <andrew.morgan@mongodb.com>
2025-08-27 09:23:48 +0000

    Add link styling for tide times and wrap tide information in a clickable link to tidecharts.com

--- src/components/footer/Tide/TideTime.jsx
+++ src/components/footer/Tide/TideTime.jsx
@@ -7,21 +7,22 @@ export default function TideTime({ tide }) {
 
   return (
     <div className="tide-time">
-      <span className={tide.next === true ? 'tide-label-next' : 'tide-label'}>
-          <i className={tide.status === 'high' ? 'fa fa-angle-double-up' : 
-            'fa fa-angle-double-down'
-          }></i> 
-          {language === 'fr' ? ` ${tide.dayOfWeekFrench} ` : 
-            ` ${tide.dayOfWeekEnglish} `
-          }
-          {`${tide.time}: `}
-          {`${tide.status === 'low' ? (
-              language === 'fr' ? 'Marée basse' : 'Low tide') : (
-              language === 'fr' ? 'Marée haute' : 'High tide') 
-            }`
-          }
-      </span>
-
+      <a href="https://www.tideschart.com/France/Nouvelle--Aquitaine/Charente--Maritime/Ile-d_Oleron----Vert-Bois_Les-Allassins/Weekly/" className="no-colour">
+        <span className={tide.next === true ? 'tide-label-next' : 'tide-label'}>
+            <i className={tide.status === 'high' ? 'fa fa-angle-double-up' : 
+              'fa fa-angle-double-down'
+            }></i> 
+            {language === 'fr' ? ` ${tide.dayOfWeekFrench} ` : 
+              ` ${tide.dayOfWeekEnglish} `
+            }
+            {`${tide.time}: `}
+            {`${tide.status === 'low' ? (
+                language === 'fr' ? 'Marée basse' : 'Low tide') : (
+                language === 'fr' ? 'Marée haute' : 'High tide') 
+              }`
+            }
+        </span>
+      </a>
     </div>
   );
 }
